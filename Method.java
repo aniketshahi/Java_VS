b12e0afdac7bb4e4ef037f78a2033ab2c5398091
Aniket Shahi <aniketshahi7766@gmail.com>
2022-09-05 21:12:43 +0530

    Method

--- Method.java
+++ Method.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Method {
 
-    static int logic(int x, int y) {  //defining method
+    static int logic(int x, int y) { // defining method
         int z;
         if (x > y) {
             z = x + y;
@@ -17,10 +17,11 @@ public class Method {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter number 1:");
-        int a=s.nextInt();
+        int a = s.nextInt();
         System.out.print("Enter number 2:");
-        int b=s.nextInt();
-        int c=logic(a,b);  //calling method
+        int b = s.nextInt();
+        int c = logic(a, b); // calling method
         System.out.println(c);
+
     }
 }
