03af378a2f38c6d0d793ec208b43fde347dde9d9
Aniket Shahi <aniketshahi7766@gmail.com>
2022-11-10 20:22:45 +0530

    new file:   Pratice$Employee.class

--- Pratice.java
+++ Pratice.java
@@ -1,34 +1,91 @@
 import java.util.Scanner;
 
 public class Pratice {
-    // // addition using add method 
-    // static int  Add(int a,int b){                  //initilizing method
-    //     System.out.print("Addition is:");
-    //     System.out.println(a+b);              
-    //     return 0;
+    // // addition using add method
+    // static int Add(int a,int b){ //initilizing method
+    // System.out.print("Addition is:");
+    // System.out.println(a+b);
+    // return 0;
     // }
-    // static void  Pratice(){
-    //        System.out.println("This is constructor");
+    // static void Pratice(){
+    // System.out.println("This is constructor");
     // }
-    
-    // public static void main(String[] args) {
-    //     Scanner sc=new Scanner(System.in);
-    //    System.out.print("Enter a:");
-    //    int  a=sc.nextInt();
-    //    System.out.print("Enter b:");
-    //    int b=sc.nextInt();
-    //    Add(a, b);                                //calling method
-    //    Pratice();
 
-   // }
-//     static void  Pratice(){                     //Initilizing constructor
-//         System.out.println("This is constructor"); 
-//  }
- 
-//  public static void main(String[] args) {
-//      Scanner sc=new Scanner(System.in);
-                    
-//     Pratice();                               //calling constructor
-//  }
+    // public static void main(String[] args) {
+    // Scanner sc=new Scanner(System.in);
+    // System.out.print("Enter a:");
+    // int a=sc.nextInt();
+    // System.out.print("Enter b:");
+    // int b=sc.nextInt();
+    // Add(a, b); //calling method
+    // Pratice();
+
+    // }
+
+    // //constructor
+    // static void Pratice(){ //Initilizing constructor
+    // System.out.println("This is constructor");
+    // }
+
+    // public static void main(String[] args) {
+    // Scanner sc=new Scanner(System.in);
+
+    // Pratice(); //calling constructor
+    // }
+
+    // //Array
+    // static void printArray(int [] a){ //passing array as argument in method
+    // for(int i=0;i<a.length;i++){
+
+    // System.out.println(a[i]);
+    // }
+    // }
+    // static int[] returnarray(){
+    // int arr[]=new int []{0,0,0,0,4,6,7,8,9};
+    // return arr; //returning array;
+    // }
+    // public static void main(String [] args){
+    // //int arr[]=new int []{1,2,3,4,4,6,7,8,9};
+    // int arr[]= returnarray();
+
+    // printArray(arr) ;
+    // returnarray();
+    // }
+
+    // //Inheritence
+
+    // //Example 1
+    // static class Employee { //superclass
+    // float salary = 100000;
+    // }
+
+    // static class Programmer extends Employee { //subclass
+    // int bonus = 20000;
+    // }
+
+    // public static void main(String[] args) {
+    // Programmer p = new Programmer();
+    // System.out.println("salary:"+p.salary);
+    // System.out.print("Bonus:"+p.bonus);
+    // }
+
+    // //example 2
+
+    // static class Animal{
+    // void printMessag(){
+    // System.out.println("Animal Mtehod");
+    // }
+    // }
+    // static class dogs extends Animal{
+
+    // void printMessage(){
+    // printMessag();
+    // System.out.println("Bark");
+    // }
+    // }
+    // public static void main(String[] args) {
+    // dogs d=new dogs();
+    // d.printMessage();
+    // }
 
 }
