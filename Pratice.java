93660b2d24a922fa655f44a9fd99cbe89f394034
Aniket Shahi <aniketshahi7766@gmail.com>
2022-11-10 19:31:32 +0530

    new file:   Pratice.class

---
+++ Pratice.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Pratice {
+    // // addition using add method 
+    // static int  Add(int a,int b){                  //initilizing method
+    //     System.out.print("Addition is:");
+    //     System.out.println(a+b);              
+    //     return 0;
+    // }
+    // static void  Pratice(){
+    //        System.out.println("This is constructor");
+    // }
+    
+    // public static void main(String[] args) {
+    //     Scanner sc=new Scanner(System.in);
+    //    System.out.print("Enter a:");
+    //    int  a=sc.nextInt();
+    //    System.out.print("Enter b:");
+    //    int b=sc.nextInt();
+    //    Add(a, b);                                //calling method
+    //    Pratice();
+
+   // }
+//     static void  Pratice(){                     //Initilizing constructor
+//         System.out.println("This is constructor"); 
+//  }
+ 
+//  public static void main(String[] args) {
+//      Scanner sc=new Scanner(System.in);
+                    
+//     Pratice();                               //calling constructor
+//  }
+
+}
