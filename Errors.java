de808192ef6f603172843dbfddfa98601982dc4a
aniket0608 <aniketshahi7766@gmail.com>
2023-09-17 19:59:10 +0530

    deleted:    Abc.class

--- Errors.java
+++ Errors.java
@@ -1,10 +1,36 @@
-import java.util.Scanner;
-public class Errors {
-    public static void main(String[] args){
-    Scanner sc= new Scanner(System.in);
-    for(int i=0;i<10;i++){
-      System.out.println(2*i+1);
-    }
+// import java.util.Scanner;
+// public class Errors {
+//     public static void main(String[] args){
+//     Scanner sc= new Scanner(System.in);
+//     for(int i=0;i<10;i++){
+//       System.out.println(2*i+1);
+//     }
     
-    }
+//     }
+// }
+import java.util.Scanner;
+class a{
+  int a;
+  int b;
+ public int a(int a,int b){
+    int c;
+    c=a+b;
+    System.out.println("Sum is :"+c);
+    return 0;
+  }
+  public int a(int a,int b,int c){
+    int d;
+    d=a+b+c;
+    System.out.println("Sum is :"+d);
+    return 0;
+  }
+}
+   public class Errors{
+  public static void main(String[] args){
+    System.out.println("Hello");
+    a obj=new a();
+    obj.a(4, 9);
+    obj.a(3, 5, 8);
+    
+  }
 }
---
+++ Final.java
@@ -0,0 +1,54 @@
+public class Final {
+    public static void main(String[] args) {
+        final String Name = "Digvijay Singh";
+        System.out.println("Name" + "=" + Name);
+        final int Roll = 38;
+        System.out.println("Roll" + "=" + Roll);
+        final int STUDENT_MARKS = 100;
+        System.out.println("STUDENT_MARKS" + "=" + STUDENT_MARKS);
+        final String Teacher;
+
+        {
+            String Teacher1 = "Harish Sir";
+            System.out.println("Teacher" + "=" + Teacher1);
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ]
